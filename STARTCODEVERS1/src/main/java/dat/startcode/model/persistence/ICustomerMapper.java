c32e230f18d1636993c22632e6dfe8978c8acd2a
Sofiadoingcode <88271469+Sofiadoingcode@users.noreply.github.com>
2022-05-09 12:58:48 +0200

    GetallAcceptedOrders

--- STARTCODEVERS1/src/main/java/dat/startcode/model/persistence/ICustomerMapper.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/model/persistence/ICustomerMapper.java
@@ -3,10 +3,12 @@ package dat.startcode.model.persistence;
 import dat.startcode.model.entities.Customer;
 import dat.startcode.model.exceptions.DatabaseException;
 
+import java.util.List;
+
 public interface ICustomerMapper {
 
     public Customer getAllNonAcceptedRequests() throws DatabaseException;
-    public Customer getAllAcceptedRequests() throws DatabaseException;
+    public List<Customer> getAllAcceptedRequests() throws DatabaseException;
     public Customer getAllPaidRequests() throws DatabaseException;
 
 
