5c110fe7fbff8de6e79762c33feb1da99779f0df
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2022-05-09 12:58:10 +0200

    Adds getAllNonAccepted to customerMapper

--- STARTCODEVERS1/src/main/java/dat/startcode/model/persistence/ICustomerMapper.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/model/persistence/ICustomerMapper.java
@@ -3,9 +3,11 @@ package dat.startcode.model.persistence;
 import dat.startcode.model.entities.Customer;
 import dat.startcode.model.exceptions.DatabaseException;
 
+import java.util.List;
+
 public interface ICustomerMapper {
 
-    public Customer getAllNonAcceptedRequests() throws DatabaseException;
+    public List<Customer> getAllNonAcceptedRequests() throws DatabaseException;
     public Customer getAllAcceptedRequests() throws DatabaseException;
     public Customer getAllPaidRequests() throws DatabaseException;
 
