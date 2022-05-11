fa833c14ba1c18b9e61faf2f4cdd0e6543cfff0e
ヾ(≧∇≦*)ゝ <krisdanoda@users.noreply.github.com>
2022-05-11 21:45:33 +0200

    facade done

--- STARTCODEVERS1/src/main/java/dat/startcode/test.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/test.java
@@ -5,6 +5,7 @@ import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
 import dat.startcode.model.persistence.CustomerMapper;
 import dat.startcode.model.persistence.ProductMapper;
+import dat.startcode.model.services.ProductionFacade;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,18 @@ public class test {
     public static void main(String[] args) {
        ConnectionPool connectionPool = new ConnectionPool();
        ProductMapper productMapper = new ProductMapper(connectionPool);
+
+
+       try {
+           ProductionFacade.createProduct("meganut","Beslag & Skruer","stk",3,100,100,5,connectionPool);
+       }catch (Exception e){
+
+           System.out.println(e);
+
+       }
+
+
+        /*
         try {
             productMapper.createProduct("deeznuts", "Beslag & Skruer", "stk", 2, 5100, 5100, 69);
         }catch (Exception e){
@@ -21,7 +34,7 @@ public class test {
             System.out.println("lol");
         }
 
-
+*/
 
     }
 
