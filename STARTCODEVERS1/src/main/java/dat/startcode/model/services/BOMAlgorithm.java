32f6a6a7101eb67c84cb4944a28331d1785b5353
Magical Glove <90389752+MagicalGlove@users.noreply.github.com>
2022-05-17 20:39:39 +0200

    Done with StolpeAlgo & HulbåndAlgo

--- STARTCODEVERS1/src/main/java/dat/startcode/model/services/BOMAlgorithm.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/model/services/BOMAlgorithm.java
@@ -15,14 +15,16 @@ import java.util.List;
 
 public class BOMAlgorithm {
     private int spærHeight = 0;
-    private int spærAntal = 0;
+    private int spærAntal = 14;
     private int tagHeight = 0;
-    private int antalStolper = 0;
-    private double length = 0;
     private ConnectionPool connectionPool;
 
     public BOMAlgorithm() {
         this.connectionPool = ApplicationStart.getConnectionPool();
+    }
+
+    public BOMAlgorithm(ConnectionPool connectionPool) throws DatabaseException {
+        this.connectionPool = connectionPool;
     }
 
 
@@ -61,7 +63,7 @@ public class BOMAlgorithm {
     }
 
 
-    private List<ProductDTO> loadAllProducts() {
+    public List<ProductDTO> loadAllProducts() {
         List<ProductDTO> allproducts = new ArrayList<>();
 
         BOMMapper bomMapper = new BOMMapper(connectionPool);
@@ -148,23 +150,77 @@ public class BOMAlgorithm {
         return onlyThisItemProductionlines;
     }
 
-    private List<ProductLine> calculateStolpeProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
+    public List<ProductLine> calculateStolpeProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
         List<ProductLine> returnList = new ArrayList<>();
         List<ProductDTO> allStolper = getAllNeededProducts(allproducts, "stolpe");
+        System.out.println("Alle stolper: " + allStolper.size());
+        System.out.println("Stolpetype: " + allStolper.get(0));
+        HashMap<Integer, Integer> lengths = loadAllLengths();
+        HashMap<Integer, Integer> allLargerLengths = new HashMap<>();
 
         double carportLengthWithoutExtra = carportLength - 1.1;
-        int stolpeLength = (int)carportHeight - spærHeight - tagHeight;
+
+        int stolpeLength = (int) carportHeight - spærHeight - tagHeight;
+        int stolpeId = 0;
+
+        for (Integer key : lengths.keySet()) {
+            int value = lengths.get(key);
+            if (value >= stolpeLength) {
+                allLargerLengths.put(key, value);
+            }
+
+        }
+
+        int minimumValue = Integer.MAX_VALUE;
+        int minimumValueKey = 0;
+        for (Integer key : allLargerLengths.keySet()) {
+            int value = lengths.get(key);
+
+            if (value < minimumValue) {
+                minimumValue = value;
+                minimumValueKey = key;
+            }
+
+        }
+
+        int lengthID = minimumValueKey;
+        double lengthSide = 0;
+        double lengthBack = 0;
+        int antalStolperCorner = 4;
+        int antalStolperSide = 0;
+        int antalStolperBack = 0;
+        int antalStolperOVERALL = 0;
 
         while (true) {
-            length += 3;
-            if (length < carportLengthWithoutExtra) {
-                antalStolper++;
+            lengthSide += 300;
+            if (lengthSide < carportLengthWithoutExtra) {
+                antalStolperSide++;
             } else {
                 break;
             }
         }
+        antalStolperSide *= 2;
+
+        while (true) {
+            lengthBack += 300;
+            if (lengthBack < carportWidth) {
+                antalStolperBack++;
+            } else {
+                break;
+            }
+        }
+        antalStolperOVERALL = antalStolperCorner + antalStolperSide + antalStolperBack;
+
+        for (int i = 0; i < allStolper.size(); i++) {
+
+            if (allStolper.get(i).getName().equals("trykimp. Stolpe")) {
+                stolpeId = allStolper.get(i).getIdproduct();
+                break;
+            }
+        }
 //        int productId = getAllNeededProducts();
-        ProductLine stolpeTid = new ProductLine(, antalStolper, stolpeLength, null);
+        double totalStolpePrice = calculateTotalProductPrice(allproducts, stolpeId, antalStolperOVERALL, minimumValue);
+        ProductLine stolpeTid = new ProductLine(stolpeId, antalStolperOVERALL, lengthID, totalStolpePrice);
         returnList.add(stolpeTid);
         return returnList;
     }
@@ -190,40 +246,72 @@ public class BOMAlgorithm {
         return returnList;
     }
 
-    private List<ProductLine> calculateHulbåndProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
+    public List<ProductLine> calculateHulbåndProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
         List<ProductLine> returnList = new ArrayList<>();
-        List<ProductDTO> allStolper = getAllNeededProducts(allproducts, "hulbånd");
-        double maengdeHulbaand = 0;
-        int carportDiagonal = (int)(carportLength*carportHeight)*(int)(carportWidth*carportWidth);
-        if(carportDiagonal<5){
+        List<ProductDTO> allHulbånd = getAllNeededProducts(allproducts, "hulbånd");
+        List<ProductDTO> allSkruer = getAllNeededProducts(allproducts, "skrue");
+        int beslagsskrueId = 0;
+        int hulbåndId = 0;
+        int antalBeslagsSkruer = spærAntal * 4;
+        int maengdeHulbaand = 0;
+        double carportDiagonalIAnden = (double) (carportLength * carportLength) + (double) (carportWidth * carportWidth);
+        int carportDiagonal = (int)Math.sqrt(carportDiagonalIAnden); //I centimeter
+//        carportDiagonal /=100;
+        if (carportDiagonal < 500) {
             maengdeHulbaand = 1;
-        } else if (5 < carportDiagonal && carportDiagonal < 10){
+        } else if (500 < carportDiagonal && carportDiagonal < 1000) {
             maengdeHulbaand = 2;
+        } else {
+            maengdeHulbaand = 0;
         }
 
-        ProductLine antalHulbaand = new ProductLine(, maengdeHulbaand, 0, );
+        for (int i = 0; i < allHulbånd.size(); i++) {
+
+            if (allHulbånd.get(i).getName().equals("hulbånd 1x20 mm. 10 mtr.")) {
+                hulbåndId = allHulbånd.get(i).getIdproduct();
+                break;
+            }
+        }
+
+
+        for (int i = 0; i < allSkruer.size(); i++) {
+
+            if (allSkruer.get(i).getName().equals("beslagskruer")) {
+                beslagsskrueId = allSkruer.get(i).getIdproduct();
+                break;
+            }
+        }
+
+        antalBeslagsSkruer = (int)Math.ceil((double)antalBeslagsSkruer/200);
+
+        double totalHulbåndPrice = calculateTotalProductPrice(allproducts, hulbåndId, maengdeHulbaand, 0);
+        double totalBeslagsskruerPrice = calculateTotalProductPrice(allproducts, beslagsskrueId, antalBeslagsSkruer, 0);
+
+        ProductLine antalHulbaand = new ProductLine(hulbåndId, maengdeHulbaand, 0, totalHulbåndPrice);
         returnList.add(antalHulbaand);
-        int antalBeslagsSkruer = spærAntal*4;
-        ProductLine beslagsskruer = new ProductLine(, antalBeslagsSkruer, 0, );
+        ProductLine beslagsskruer = new ProductLine(beslagsskrueId, antalBeslagsSkruer, 0, totalBeslagsskruerPrice);
         returnList.add(beslagsskruer);
         return returnList;
     }
 
-    private List<ProductLine> calculateSternProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
+    private List<ProductLine> calculateSternProductLines(List<ProductDTO> allproducts, double carportHeight,
+                                                         double carportWidth, double carportLength) {
         List<ProductLine> returnList = new ArrayList<>();
 
 
         return returnList;
     }
 
-    private List<ProductLine> calculateTagProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
+    private List<ProductLine> calculateTagProductLines(List<ProductDTO> allproducts, double carportHeight,
+                                                       double carportWidth, double carportLength) {
         List<ProductLine> returnList = new ArrayList<>();
 
 
         return returnList;
     }
 
-    private List<ProductLine> calculateVandbrætProductLines(List<ProductDTO> allproducts, double carportHeight, double carportWidth, double carportLength) {
+    private List<ProductLine> calculateVandbrætProductLines(List<ProductDTO> allproducts, double carportHeight,
+                                                            double carportWidth, double carportLength) {
         List<ProductLine> returnList = new ArrayList<>();
 
 
@@ -244,12 +332,12 @@ public class BOMAlgorithm {
         return returnList;
     }
 
-    public double calculateTotalProductPrice (List<ProductDTO> allproducts, int productID, int amount, int length) {
+    public double calculateTotalProductPrice(List<ProductDTO> allproducts, int productID, int amount, int length) {
         double totalProductPrice = 0;
 
         double priceMeasurment = 0;
-        for(ProductDTO p: allproducts) {
-            if(p.getIdproduct() == productID) {
+        for (ProductDTO p : allproducts) {
+            if (p.getIdproduct() == productID) {
                 priceMeasurment = p.getPricemeasurment();
             }
 
@@ -257,7 +345,11 @@ public class BOMAlgorithm {
 
         double lengthDouble = (double) length;
         double amountDouble = (double) amount;
-        totalProductPrice = priceMeasurment*(lengthDouble/100)*amountDouble;
+        if (length == 0) {
+            totalProductPrice = priceMeasurment * amountDouble;
+        } else {
+            totalProductPrice = priceMeasurment * (lengthDouble / 100) * amountDouble;
+        }
 
         return totalProductPrice;
     }
--- STARTCODEVERS1/src/main/java/dat/startcode/test.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/test.java
@@ -1,10 +1,14 @@
 package dat.startcode;
 
+import dat.startcode.control.GenerateBOM;
+import dat.startcode.model.DTOs.ProductDTO;
 import dat.startcode.model.entities.Product;
+import dat.startcode.model.entities.ProductLine;
 import dat.startcode.model.exceptions.DatabaseException;
 import dat.startcode.model.persistence.ConnectionPool;
 import dat.startcode.model.persistence.CustomerMapper;
 import dat.startcode.model.persistence.ProductMapper;
+import dat.startcode.model.services.BOMAlgorithm;
 import dat.startcode.model.services.ProductionFacade;
 
 import java.util.ArrayList;
@@ -12,20 +16,22 @@ import java.util.List;
 
 public class test {
 
-    public static void main(String[] args) {
-       ConnectionPool connectionPool = new ConnectionPool();
-
-        try {
-
-            ProductionFacade.createProduct("hej","Træ & Tagplader", "sæt", 10,10,10,10,"rem",connectionPool);
+    public static void main(String[] args) throws DatabaseException {
+        ConnectionPool connectionPool = new ConnectionPool();
 
 
-        }catch (Exception e){
+            BOMAlgorithm bomAlgorithm = new BOMAlgorithm(connectionPool);
 
-            System.out.println("something went wrong");
+            List<ProductDTO> allproducts = bomAlgorithm.loadAllProducts();
 
-        }
+                List<ProductLine> yeet = bomAlgorithm.calculateHulbåndProductLines(allproducts, 400,300, 500);
 
+            for (ProductLine productLine: yeet) {
+                System.out.println("ProductID: " + productLine.getProductID());
+                System.out.println("LengthID: " + productLine.getLengthID());
+                System.out.println("ProductPrice: " + productLine.getTotalproductprice());
+                System.out.println("Amount: " + productLine.getAmount());
+            }
 
 
 
