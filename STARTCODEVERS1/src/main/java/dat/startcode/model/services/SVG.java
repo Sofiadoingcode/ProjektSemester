e99221f511c50fc911d1e09bda1795ff58fbb478
SMerved <90383268+SMerved@users.noreply.github.com>
2022-05-18 15:15:44 +0200

    Update SVG.java

--- STARTCODEVERS1/src/main/java/dat/startcode/model/services/SVG.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/model/services/SVG.java
@@ -1,4 +1,24 @@
 package dat.startcode.model.services;
 
 public class SVG {
+    StringBuilder svg = new StringBuilder();
+
+    private int x;
+    private int y;
+    private String viewBox;
+    private int width;
+    private int height;
+
+    private final String headerTemplate = "<svg height=\"690\" width=\"855\" " +
+            "viewBox=\"0 0 855 690\"\n" +
+            "preserveAspectRatio=\"xMinYMin\">";
+
+    public SVG(int x, int y, String viewBox, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.viewBox = viewBox;
+        this.width = width;
+        this.height = height;
+        svg.append("<svg>.....");
+    }
 }
