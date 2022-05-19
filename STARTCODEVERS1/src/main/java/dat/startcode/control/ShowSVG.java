061ba56dd676da76fdc6f0a959b1d4b9dfa27d0e
SMerved <90383268+SMerved@users.noreply.github.com>
2022-05-19 10:40:02 +0200

    y

--- STARTCODEVERS1/src/main/java/dat/startcode/control/ShowSVG.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/control/ShowSVG.java
@@ -1,5 +1,7 @@
 package dat.startcode.control;
 
+import dat.startcode.model.services.SVG;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,8 +11,14 @@ public class ShowSVG extends Command
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response)
     {
+        SVG svg = new SVG(0,0, "0 0 800 600", 100, 100);
+
+        for(int x = 0; x < 14 ; x++) {
+            svg.addRect(100 + 50 * x, 0, 600, 4.5);
+        }
+
         HttpSession session = request.getSession();
-        request.setAttribute("svgdrawing", "tegning");
+        request.setAttribute("svgdrawing", svg.toString());
         return "svgtegning.jsp";
     }
 }
--- STARTCODEVERS1/src/main/java/dat/startcode/model/services/SVG.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/model/services/SVG.java
@@ -6,12 +6,18 @@ public class SVG {
     private int x;
     private int y;
     private String viewBox;
-    private int width;
-    private int height;
+    private double width;
+    private double height;
 
-    private final String headerTemplate = "<svg height=\"690\" width=\"855\" " +
-            "viewBox=\"0 0 855 690\"\n" +
+    private final String headerTemplate = "<svg height=\"%s%%\" " +
+            "width=\"%s%%\" " +
+            "viewBox=\"%s\"" +
+            "x=\"%s\"" +
+            "y=\"%s\"" +
             "preserveAspectRatio=\"xMinYMin\">";
+
+    private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%f\" width=\"%f\"\n" +
+            "          style=\"stroke:#000000; fill: #ffffff\"/>";
 
     public SVG(int x, int y, String viewBox, int width, int height) {
         this.x = x;
@@ -19,6 +25,22 @@ public class SVG {
         this.viewBox = viewBox;
         this.width = width;
         this.height = height;
-        svg.append("<svg>.....");
+        svg.append(String.format(headerTemplate, height, width, viewBox, x, y));
+    }
+
+    public void addRect(int x, int y, double height, double width){
+        svg.append(String.format(rectTemplate, x, y, height, width));
+    }
+
+    public void addLine(int x1, int y1, int x2, int y2){
+        svg.append(String.format(rectTemplate, x, y, height, width));
+    }
+
+    public void addSvg(SVG innerSVG){
+        svg.append(innerSVG.toString());
+    }
+
+    public String toString(){
+        return svg.toString() + "</svg>";
     }
 }
