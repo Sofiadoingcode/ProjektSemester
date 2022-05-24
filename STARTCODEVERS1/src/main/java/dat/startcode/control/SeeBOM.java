97f21b77f12b4e894cda54adb0b12b6f62200c14
SMerved <90383268+SMerved@users.noreply.github.com>
2022-05-24 09:38:40 +0200

    Update SeeBOM.java

--- STARTCODEVERS1/src/main/java/dat/startcode/control/SeeBOM.java
+++ STARTCODEVERS1/src/main/java/dat/startcode/control/SeeBOM.java
@@ -1,7 +1,7 @@
 package dat.startcode.control;
 
 import dat.startcode.model.DTOs.BOMDTO;
-import dat.startcode.model.DTOs.ProductionlineDTO;
+import dat.startcode.model.DTOs.ProductionLineDTO;
 import dat.startcode.model.config.ApplicationStart;
 import dat.startcode.model.entities.ProductLine;
 import dat.startcode.model.entities.Request;
@@ -28,12 +28,12 @@ public class SeeBOM extends Command {
         BOMMapper bomMapper = new BOMMapper(connectionPool);
         HttpSession session = request.getSession();
         Request usersRequest = (Request) session.getAttribute("usersRequest");
-        BOMDTO bomdto = bomMapper.getBOM(usersRequest.getIdorder());
-        List<ProductionlineDTO> productionlines = bomMapper.getBOMProductlines(bomdto);
+        BOMDTO bomdto = bomMapper.getBOM(usersRequest.getIdOrder());
+        List<ProductionLineDTO> productionlines = bomMapper.getBOMProductLines(bomdto);
 
-        List<ProductionlineDTO> category1 = new ArrayList<>();
-        List<ProductionlineDTO> category2 = new ArrayList<>();
-        for(ProductionlineDTO dto: productionlines) {
+        List<ProductionLineDTO> category1 = new ArrayList<>();
+        List<ProductionLineDTO> category2 = new ArrayList<>();
+        for(ProductionLineDTO dto: productionlines) {
             if (dto.getCategory() == 1) {
                 category1.add(dto);
             } else {
